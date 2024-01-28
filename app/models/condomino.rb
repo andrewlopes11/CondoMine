b8da2019406a5b71778d948547ad418ca2f5e7e5
Andrew Lopes Leal <andrewlopes755433@gmail.com>
2024-01-28 11:59:58 -0300

    validates condomino: nome, cpf e contato

--- app/models/condomino.rb
+++ app/models/condomino.rb
@@ -1,2 +1,5 @@
 class Condomino < ApplicationRecord
+  validates :nome, presence: true, length: {minimum: 5}, numericality: false
+  validates :cpf, presence: true, length: {maximum: 11}, uniqueness: true, numericality: {only_integer: true}
+  validates :contato, presence: true, uniqueness: true, confirmation: { case_sensitive: false }
 end
